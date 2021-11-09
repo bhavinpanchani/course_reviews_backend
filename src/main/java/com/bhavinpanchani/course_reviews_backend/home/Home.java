b0cf7184baa0fc8f04b83a0a534dcb4333cd8b66
bhavinpanchani <bhavinpanchani9@gmail.com>
2021-11-09 18:59:13 -0500

    This changes will be added first in Test-Branch

--- src/main/java/com/bhavinpanchani/course_reviews_backend/home/Home.java
+++ src/main/java/com/bhavinpanchani/course_reviews_backend/home/Home.java
@@ -8,7 +8,7 @@ public class Home {
 
 	@GetMapping("/")
 	public String homePage() {
-		return "Welcome to StudyON - Find all details for Universities, Majors, Courses, Professors";
+		return "Welcome to StudyON - Find all details for Universities, Majors, Courses, Professors. Created new Branch named Test-Branch in Github";
 	}
 	
 }
