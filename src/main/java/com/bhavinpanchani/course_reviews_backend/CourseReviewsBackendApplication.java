a3b31bf8f47dcb33c96079b4680a71e2c2d4a97c
bhavinpanchani <bhavinpanchani9@gmail.com>
2022-09-21 18:24:59 -0400

    Adding cors config

--- src/main/java/com/bhavinpanchani/course_reviews_backend/CourseReviewsBackendApplication.java
+++ src/main/java/com/bhavinpanchani/course_reviews_backend/CourseReviewsBackendApplication.java
@@ -2,6 +2,9 @@ package com.bhavinpanchani.course_reviews_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class CourseReviewsBackendApplication {
@@ -9,5 +12,15 @@ public class CourseReviewsBackendApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CourseReviewsBackendApplication.class, args);
 		
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/university/**").allowedOrigins("http://127.0.0.1:3000", "http://localhost:3000", "https://bhavinpanchani.github.io");
+			}
+		};
 	}
 }
